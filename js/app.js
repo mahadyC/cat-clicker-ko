778f5d88f79936dccd2dcfb071d630df41edf822
mahadyC <mahady.chowdhury@metropolia.fi>
2017-06-04 20:08:41 +0300

    Initialize model inside the viewModel

--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
 var ViewModel = function(){
-
+	this.clickCount = ko.observable(0);
+	this.name = ko.observable("fluffy");
+	this.imgSrc = ko.observable("img/22252709_010df3379e_z.jpg");
 };
 
 ko.applyBindings(new ViewModel());
