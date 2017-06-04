1749759b709ba083a54ae1e7ef6b0fee1cc9b8b9
mahadyC <mahady.chowdhury@metropolia.fi>
2017-06-04 20:18:01 +0300

    Add increment counter function to ViewModel

--- js/app.js
+++ js/app.js
@@ -2,6 +2,10 @@ var ViewModel = function(){
 	this.clickCount = ko.observable(0);
 	this.name = ko.observable("fluffy");
 	this.imgSrc = ko.observable("img/22252709_010df3379e_z.jpg");
+
+	this.incrementCounter = function(){
+		this.clickCount(this.clickCount() + 1);
+	};
 };
 
 ko.applyBindings(new ViewModel());
