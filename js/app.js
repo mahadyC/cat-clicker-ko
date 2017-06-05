17a5d9877d2857827b360603a359b52c22395ff4
mahadyC <mahady.chowdhury@metropolia.fi>
2017-06-05 12:53:11 +0300

    Add cat level computed observable

--- js/app.js
+++ js/app.js
@@ -6,6 +6,12 @@ var ViewModel = function(){
 	this.incrementCounter = function(){
 		this.clickCount(this.clickCount() + 1);
 	};
+
+	this.catLevel = ko.computed(function(){
+		if(this.clickCount < 10){
+			return "new born";
+		}
+	}, this);
 };
 
 ko.applyBindings(new ViewModel());
