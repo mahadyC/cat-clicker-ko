18905f3389fd9c9bdc5b94a4681f121b259eda0f
mahadyC <mahady.chowdhury@metropolia.fi>
2017-06-07 13:04:50 +0300

    Add observable array consisting nicknames

--- js/app.js
+++ js/app.js
@@ -2,7 +2,10 @@ var ViewModel = function(){
 	this.clickCount = ko.observable(0);
 	this.name = ko.observable("fluffy");
 	this.imgSrc = ko.observable("img/22252709_010df3379e_z.jpg");
-
+	this.nickName = ko.observableArray([
+		"Tammy", "Chuci", "Mew", "Arr"
+	]);
+	
 	this.incrementCounter = function(){
 		this.clickCount(this.clickCount() + 1);
 	};
@@ -16,6 +19,8 @@ var ViewModel = function(){
 			return "Teen";
 		}
 	}, this);
+
+
 };
 
 ko.applyBindings(new ViewModel());
