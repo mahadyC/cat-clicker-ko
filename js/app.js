37eae58d7a3582d1266b08aa4ac19ba904f0f293
mahadyC <mahady.chowdhury@metropolia.fi>
2017-06-07 13:47:05 +0300

    Debug loading error

--- js/app.js
+++ js/app.js
@@ -2,9 +2,11 @@ var ViewModel = function(){
 	this.clickCount = ko.observable(0);
 	this.name = ko.observable("fluffy");
 	this.imgSrc = ko.observable("img/22252709_010df3379e_z.jpg");
-	this.nickName = ko.observableArray({
-		names:["Tammy", "Chuci", "Mew", "Arr"]
-	});
+	this.nickName = ko.observableArray([
+		{
+			names:["Tammy", "Chuci", "Mew", "Arr"]
+		}
+	]);
 
 	this.incrementCounter = function(){
 		this.clickCount(this.clickCount() + 1);
