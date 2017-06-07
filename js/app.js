701777ff37f999924edad9c19823ffe6f2af9f37
mahadyC <mahady.chowdhury@metropolia.fi>
2017-06-07 13:23:41 +0300

    Modify to use in foreach

--- js/app.js
+++ js/app.js
@@ -2,10 +2,10 @@ var ViewModel = function(){
 	this.clickCount = ko.observable(0);
 	this.name = ko.observable("fluffy");
 	this.imgSrc = ko.observable("img/22252709_010df3379e_z.jpg");
-	this.nickName = ko.observableArray([
-		"Tammy", "Chuci", "Mew", "Arr"
-	]);
-	
+	this.nickName = ko.observableArray({
+		names:["Tammy", "Chuci", "Mew", "Arr"]
+	});
+
 	this.incrementCounter = function(){
 		this.clickCount(this.clickCount() + 1);
 	};
