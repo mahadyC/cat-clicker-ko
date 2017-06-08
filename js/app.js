c087abf3031e2a90d32c532f00cd7c75157abd1c
mahadyC <mahady.chowdhury@metropolia.fi>
2017-06-08 10:52:59 +0300

    Introduce cat function to represent as model and moving the model from view model to cat function

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
-var ViewModel = function(){
+var Cat = function(){
+	
 	this.clickCount = ko.observable(0);
 	this.name = ko.observable("fluffy");
 	this.imgSrc = ko.observable("img/22252709_010df3379e_z.jpg");
@@ -9,10 +10,6 @@ var ViewModel = function(){
 		{ name: "Glu"}
 	]);
 
-	this.incrementCounter = function(){
-		this.clickCount(this.clickCount() + 1);
-	};
-
 	this.catLevel = ko.computed(function(){
 		if(this.clickCount() < 10){
 			return "Newborn";
@@ -22,6 +19,16 @@ var ViewModel = function(){
 			return "Teen";
 		}
 	}, this);
+};
+
+
+var ViewModel = function(){
+	
+	this.incrementCounter = function(){
+		this.clickCount(this.clickCount() + 1);
+	};
+
+	
 
 
 };
