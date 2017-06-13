2bd72dad05a6addcc2864eda29c5a89e09e9c244
mahadyC <mahady.chowdhury@metropolia.fi>
2017-06-13 13:27:32 +0300

    Get an array of cat data

--- js/app.js
+++ js/app.js
@@ -1,3 +1,37 @@
+
+var initialCats = [
+    		{
+                clickCount: 0,
+                name: "mini",
+                image: "img/22252709_010df3379e_z.jpg",
+                nickName: [{name: "Tammy"},{name: "Meow"}, {name: "Cuddly"}, {name: "Glu"}]
+            },
+            {
+            	clickCount: 0,
+                name: "bini",
+                image: "img/434164568_fea0ad4013_z.jpg",
+                nickName: [{name: "Tammy"},{name: "Meow"}, {name: "Cuddly"}, {name: "Glu"}]
+            },
+            {
+                clickCount: 0,
+                name: "sini",
+                image: "img/1413379559_412a540d29_z.jpg",
+                nickName: [{name: "Tammy"},{name: "Meow"}, {name: "Cuddly"}, {name: "Glu"}]
+            },
+            {
+                clickCount: 0,
+                name: "gini",
+                image: "img/4154543904_6e2428c421_z.jpg",
+                nickName: [{name: "Tammy"},{name: "Meow"}, {name: "Cuddly"}, {name: "Glu"}]
+            },
+            {
+                clickCount: 0,
+                name: "yini",
+                image: "img/9648464288_2516b35537_z.jpg",
+                nickName: [{name: "Tammy"},{name: "Meow"}, {name: "Cuddly"}, {name: "Glu"}]
+            }
+        ];
+
 var Cat = function(data){
 	
 	this.clickCount = ko.observable(data.clickCount);
